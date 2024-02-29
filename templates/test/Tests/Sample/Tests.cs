8281387377e3d9b075a23c9f7f33e43947a6aa87
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2024-02-29 14:44:12 +0530

    update fetest

---
+++ templates/test/Tests/Sample/Tests.cs
@@ -0,0 +1,12 @@
+namespace Tests.Sample;
+
+public class App : AppFixture<Program> {}
+
+public class Tests : TestClass<App>
+{
+    [Fact]
+    public void Sample_Test()
+    {
+        (1 + 1).Should().Be(2);
+    }
+}
